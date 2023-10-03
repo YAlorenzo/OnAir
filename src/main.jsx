719b51cf0d0240a13be3b555ed3a68997b57624b
YAlorenzo <zhenia.klimenchuk@gmail.com>
2023-10-03 17:13:51 +0200

    fix_router

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import App from './App'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './redux/store'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <Router>
          <App />
-      </BrowserRouter>
+      </Router>
        </Provider>
   </React.StrictMode>,
 )
